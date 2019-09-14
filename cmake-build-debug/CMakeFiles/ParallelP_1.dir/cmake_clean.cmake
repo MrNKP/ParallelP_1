3aa43238056678602b14fea97b69a9d5ef5f0b3d
MrNKP <45802463+MrNKP@users.noreply.github.com>
2019-09-14 23:25:45 +0300

    add cmake-debug

---
+++ cmake-build-debug/CMakeFiles/ParallelP_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParallelP_1.dir/main.cpp.obj"
+  "ParallelP_1.pdb"
+  "ParallelP_1.exe"
+  "ParallelP_1.exe.manifest"
+  "ParallelP_1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParallelP_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
